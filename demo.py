8336796fd20f86ad85984761e71470112c21ec51
Fatou <ndeyefatousene1502@gmail.com>
2023-08-31 15:49:30 +0000

    adapting the gradio to our custom model

--- demo.py
+++ demo.py
@@ -1,9 +1,24 @@
+import torch
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-    
-if __name__ == "__main__":
-    demo.launch()   
+# Load model directly
+from transformers import AutoModel, AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("Fatou/Custom-Bert-Model")
+model = AutoModel.from_pretrained("Fatou/Custom-Bert-Model")
+
+classes = ["toxic", "no toxic"]
+
+def predict(text):
+    with torch.no_grad():
+        inputs = tokenizer(text, return_tensors='pt')
+
+        output = model(**inputs)
+        pred = torch.max(output.logits, dim=1)
+    return {"indice": pred.indices.item(),
+            "classe": classes[pred.indices.item()]
+            }
+
+
+demo = gr.Interface(fn=predict, inputs="text", outputs="json")
+demo.launch()   
